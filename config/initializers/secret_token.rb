94d3a668a18c36ad958ff9e8c8750c17b11a9a3e
DreadPirateJay <jay.earwood@gmail.com>
2012-08-21 15:08:46 -0500

    Created new Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shadow::Application.config.secret_token = '347b0a416ebae1c3d1f67b8fdb43248a6a737df827d6a53332cf4ef3c2eae1b2e04ceb018d24e360d3573d6352215c2394c515b1c7deabf564b2aa95e646a7de'
